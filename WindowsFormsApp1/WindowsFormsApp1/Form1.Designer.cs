a5e93c53f7d092ce000107c439d6c445bf90ed3e
Tillmann Sänger <tillmann.saenger@gmail.com>
2022-09-20 03:06:43 +0200

    open files and switch between them

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -30,33 +30,109 @@ namespace WindowsFormsApp1
         private void InitializeComponent()
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.previusButton = new System.Windows.Forms.Button();
+            this.nextButton = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.pictureBox1.Location = new System.Drawing.Point(12, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(776, 426);
+            this.pictureBox1.Size = new System.Drawing.Size(825, 471);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 489);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(167, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Bilder Öffnen";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Multiselect = true;
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(674, 489);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "keep";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // previusButton
+            // 
+            this.previusButton.Location = new System.Drawing.Point(632, 518);
+            this.previusButton.Name = "previusButton";
+            this.previusButton.Size = new System.Drawing.Size(75, 23);
+            this.previusButton.TabIndex = 3;
+            this.previusButton.Text = "previous";
+            this.previusButton.UseVisualStyleBackColor = true;
+            this.previusButton.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // nextButton
+            // 
+            this.nextButton.Location = new System.Drawing.Point(713, 518);
+            this.nextButton.Name = "nextButton";
+            this.nextButton.Size = new System.Drawing.Size(75, 23);
+            this.nextButton.TabIndex = 4;
+            this.nextButton.Text = "next";
+            this.nextButton.UseVisualStyleBackColor = true;
+            this.nextButton.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.AutoSize = true;
+            this.button5.Location = new System.Drawing.Point(674, 547);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "seep";
+            this.button5.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1056, 610);
+            this.ClientSize = new System.Drawing.Size(854, 610);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.nextButton);
+            this.Controls.Add(this.previusButton);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button previusButton;
+        private System.Windows.Forms.Button nextButton;
+        private System.Windows.Forms.Button button5;
     }
 }
 
--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,79 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        string[] arrAllImages;
+        int imageIndex = 0;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() != DialogResult.OK) return;                // guard to prevent fail of FileDialog
+            arrAllImages = openFileDialog1.FileNames;                          // read all Files into array
+            displayImage(Direction.stay);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void displayImage(Direction direction)
+        {
+
+            switch (direction)
+            {
+                case Direction.forwards:
+                    {
+                        imageIndex++;
+                        try { pictureBox1.Image = Image.FromFile(arrAllImages[imageIndex]); }
+                        catch (Exception e) { imageIndex--; }
+                        break;
+                    }
+                case Direction.backwards:
+                    {
+                        imageIndex--;
+                        try { pictureBox1.Image = Image.FromFile(arrAllImages[imageIndex]); }
+                        catch (Exception e) { imageIndex++; }
+                        break;
+                    }
+                case Direction.stay:
+                    {
+                        try{pictureBox1.Image = Image.FromFile(arrAllImages[imageIndex]);}
+                        catch (Exception e) { }
+                        break;
+                    }
+                default:
+                    pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                    break;
+            }
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            displayImage(Direction.forwards);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            displayImage(Direction.backwards);
+        }
+
+        enum Direction
+        {
+            forwards,
+            backwards,
+            stay
         }
     }
 }
