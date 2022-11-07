dc43605990473549932613b8c215b133f4e4a822
Tillmann Sänger <Tillmann.Saenger.g8s@fh-zwickau.de>
2022-11-07 01:50:42 +0100

    listenfilter

---
+++ passwortVerdampfer/passwortVerdampfer/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿
+namespace passwortVerdampfer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(193, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(175, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "verdampfen";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(175, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Datei auswählen";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(555, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Iterationen";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(636, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "erfolgreiche Iterationen";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(617, 17);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(756, 17);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "0";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(374, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(175, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Speicherort wählen";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 48);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ passwortVerdampfer/passwortVerdampfer/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace passwortVerdampfer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string filepath = openFileDialog1.FileName;
+            Stream str = new FileStream(filepath, FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(str);
+            var arlist = new System.Collections.ArrayList();
+
+            int i = 0;
+            int j = 0;
+            while (!sr.EndOfStream)
+            {
+                i++;
+                String temp = sr.ReadLine();
+                try
+                {
+                    if ((!temp.Contains(" ")) &&
+                        (temp.Length > 9) && (
+                            (temp.Contains(",")) ||
+                            (temp.Contains(".")) ||
+                            (temp.Contains("-")) ||
+                            (temp.Contains("#")) ||
+                            (temp.Contains("+")) ||
+                            (temp.Contains("_")) ||
+                            (temp.Contains(":")) ||
+                            (temp.Contains("{")) ||
+                            (temp.Contains("|")) ||
+                            (temp.Contains("}")) ||
+                            (temp.Contains("=")) ||
+                            (temp.Contains("$")) ||
+                            (temp.Contains("(")) ||
+                            (temp.Contains(")")))
+                        && (temp.Any(char.IsUpper))
+                        && (temp.Any(char.IsLower))
+                        && (temp.Any(char.IsDigit)))
+                    {
+                        arlist.Add(temp);
+                        j++;
+                    }
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Fehler");
+                }
+            }
+            sr.Close();
+            label3.Text = i.ToString();
+            label4.Text = j.ToString();
+
+            using (TextWriter writer = File.CreateText(@"C:\Users\Tillmann\Desktop\words.txt"))
+            {
+                foreach (string actor in arlist)
+                {
+                    writer.WriteLine(actor);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+    }
+}
