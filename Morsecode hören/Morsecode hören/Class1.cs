e9c6e1bafc2ad50137655c5b50f48eb2a27dfc08
Tillmann Sänger <Tillmann.Saenger.g8s@fh-zwickau.de>
2023-01-19 23:30:12 +0100

    Morsecode durch hören Lernen

---
+++ Morsecode hören/Morsecode hören/Class1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Speech.Synthesis;
+
+namespace Morsecode_hören
+{
+    class Letter
+    {
+        private string name;
+        private string morsecode;
+
+        private static int frequenz = 800;
+        private static int ditDuration = 50;
+        private static int dahDuration = 200;
+
+        private static SpeechSynthesizer speaker;
+
+        public Letter(string name, string morsecode)
+        {
+            this.name = name;
+            this.morsecode = morsecode;
+        }
+
+        /// plays the entire letter
+        public void playLetterAsMorsecode()
+        {
+            foreach (char letter in morsecode)
+            {               
+                if (letter.ToString().Equals(".")){
+                    playDit();
+                    Console.Out.Write(letter);
+                } else if (letter.ToString().Equals("-"))
+                {
+                    playDah();
+                    Console.Out.Write(letter);
+                } else {
+                    //TODO:Eroor ausgeben
+                }
+            }
+            Console.Out.WriteLine("");
+        }
+
+        public string getName()
+        {
+            return this.name;
+        }
+
+        public string getMorsecode()
+        {
+            return this.morsecode;
+        }
+
+        public static Boolean setFreqency(int frequenz)
+        {
+            try
+            {
+                Letter.frequenz = frequenz;
+                return true;
+            }
+            catch (Exception e)
+            {
+                //TODO: throw exception
+            }
+            return false;
+        }
+
+        public static Boolean setDitDuration(int duration)
+        {
+            try{
+                Letter.ditDuration = duration;
+                return true;
+            }catch (Exception e){
+                //TODO: throw exception
+            }
+            return false;
+        }
+
+        public static Boolean setDahDuration(int duration)
+        {
+            try
+            {
+                Letter.dahDuration = duration;
+                return true;
+            }
+            catch (Exception e)
+            {
+                //TODO: throw exception
+            }
+            return false;
+        }
+
+
+        private static void playDit()
+        {
+            Console.Beep(frequenz, ditDuration);
+        }
+
+        private static void playDah()
+        {
+            Console.Beep(frequenz, dahDuration);
+        }
+        public Boolean playLetterAsSound()
+        {
+            try{
+                speaker = new SpeechSynthesizer();
+                speaker.SetOutputToDefaultAudioDevice();
+                speaker.Rate = 1;
+                speaker.Volume = 100;
+                speaker.SpeakAsync(this.getName());
+                return true;
+            }catch{
+                //TODO: throw exception
+            }
+            return false;
+        }
+    }
+}
--- Morsecode hören/Morsecode hören/Program.cs
+++ Morsecode hören/Morsecode hören/Program.cs
@@ -4,15 +4,101 @@ using System.Linq;
 using System.Media;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
 
 namespace Morsecode_hören
 {
     class Program
     {
-        static void Main(string[] args)
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        private static string[,] alphabet = new string[,]    {  { "A", ".-" },
+                                                                { "B", "-..." },
+                                                                { "C", "-.-." },
+                                                                { "D", "-.." },
+                                                                { "E", "." },
+                                                                { "F", "..-." },
+                                                                { "G", "--." },
+                                                                { "H", "...." },
+                                                                { "I", ".." },
+                                                                { "J", ".---" },
+                                                                { "K", "-.-" },
+                                                                { "L", ".-.." },
+                                                                { "M", "--" },
+                                                                { "N", "-." },
+                                                                { "O", "---" },
+                                                                { "P", ".--." },
+                                                                { "Q", "--.-" },
+                                                                { "R", ".-." },
+                                                                { "S", "..." },
+                                                                { "T", "-" },
+                                                                { "U", "..-" },
+                                                                { "V", "...-" },
+                                                                { "W", ".--" },
+                                                                { "X", "-..-" },
+                                                                { "Y", "-.--" },
+                                                                { "Z", "--.." } };
+
+     static void Main(string[] args)
         {
-            Console.Beep(800,100);
-            Console.In.ReadLine();
+            Random rnd = new Random();
+            Letter letter;
+
+            int argLength = 2;
+            int timBetweenLetters = 120000;
+            IntPtr h = Process.GetCurrentProcess().MainWindowHandle;
+
+            foreach (string arg in args)
+            {
+                string argumentPrefix = arg.Substring(0, argLength);
+                string argument = arg.Substring(argLength, arg.Length - argLength);
+
+                switch (argumentPrefix)
+                {
+                    case "-h":
+                        Console.Out.WriteLine("-h --> help");
+                        Console.Out.WriteLine("-. --> sets playtime for . duration");
+                        Console.Out.WriteLine("-- --> sets playtime for - duration");
+                        Console.Out.WriteLine("-f --> sets frequency fore played tone");
+                        Console.Out.WriteLine("-t --> sets time between letters");
+                        Console.Out.WriteLine("-v --> invisibility");
+                        break;
+                    case "-.":
+                        Letter.setDitDuration(int.Parse(argument));
+                        Console.Out.WriteLine("sets dit duration to: " + argument);
+                        break;
+                    case "--":
+                        Letter.setDahDuration(int.Parse(argument));
+                        Console.Out.WriteLine("sets dah duration to: " + argument);
+                        break;
+                    case "-f":
+                        Letter.setFreqency(int.Parse(argument));
+                        Console.Out.WriteLine("sets tone frequency to: " + argument);
+                        break;
+                    case "-t":
+                        timBetweenLetters = int.Parse(argument);
+                        Console.Out.WriteLine("sets time between letters to: " + argument);
+                        break;
+                    case "-v":
+                        Console.Out.WriteLine("sets invisibility");
+                        ShowWindow(h, 0);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            while (true)
+            {
+                int randomNuber = rnd.Next(0, 26);
+                letter = new Letter(alphabet[randomNuber,0], alphabet[randomNuber,1]);
+                letter.playLetterAsMorsecode();
+                System.Threading.Thread.Sleep(5000);
+                letter.playLetterAsSound();
+                System.Threading.Thread.Sleep(timBetweenLetters);
+            }
         }
     }
 }
