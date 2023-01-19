5de0915b1d40bcaf9047bf4ca68146840ba271cc
Tillmann Sänger <Tillmann.Saenger.g8s@fh-zwickau.de>
2023-01-19 20:47:54 +0100

    rotz aussortiert

---
+++ Morsecode hören/Morsecode hören/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Morsecode_hören
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Beep(800,100);
+            Console.In.ReadLine();
+        }
+    }
+}
